558223b7ac900bf4cf00cb12c52a3c384796748d
hitorigoya <75735750+hitorigoya@users.noreply.github.com>
2023-10-24 02:02:00 +0900

    5

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 //     return arg * 2
 // }
 
-// const abc = (arg) => {
-//     return arg * 2
-// }
-
-export function abc(arg) {
+const abc = (arg) => {
     return arg * 2
 }
 
+// export function abc(arg){
+//     return arg * 2
+// }
 
-// export default abc
+
+export default abc
